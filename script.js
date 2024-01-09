84ddba7ca9c69c05f048a83e1fd8d37e9d53e453
sityu11 <911sera@gmail.com>
2024-01-09 17:16:23 +0100

    update-240109-1716

--- script.js
+++ script.js
@@ -3,6 +3,16 @@ const products = [
     { name: "Kartondoboz E0 (78x78x82)", price: 45, quantity: "150 db", description: "Kartondoboz E0 Mérete: 78x82x82 mm", image: "image/kartondoboz-e0.jpg" },
     { name: "Kartondoboz F0 (140x190x45)", price: 100, quantity: "120 db", description: "Kartondoboz F0 Mérete: 140x190x45 mm", image: "image/kartondoboz-f0.jpg" },
     { name: "Kartondoboz F1 (140x190x60)", price: 120, quantity: "50 db", description: "Kartondoboz F1 Mérete: 140x190x60 mm", image: "image/kartondoboz-f0.jpg" },
+    { name: "Kartondoboz G9 (260x400x400)", price: 300, quantity: "150 db", description: "Kartondoboz G9 260x400x400 mm", image: "image/doboz.jpg" },
+    
+    { name: "Futártasak FB02 (225x325x50)", price: 25, quantity: "440 db", description: "Futártasak FB02 Mérete: 225x325x50 mm", image: "image/futartasak.jpg" },
+    { name: "Futártasak FB03 (240x350x50)", price: 26, quantity: "110 db", description: "Futártasak FB03 Mérete: 240x350x50 mm", image: "image/futartasak.jpg" },
+    { name: "Futártasak FB04 (325x425x50)", price: 40, quantity: "30 db", description: "Futártasak FB04 Mérete: 325x425x50 mm", image: "image/futartasak.jpg" },
+    { name: "Futártasak FB05 (350x450x50)", price: 50, quantity: "280 db", description: "Futártasak FB05 Mérete: 350x450x50 mm", image: "image/futartasak.jpg" },
+    { name: "Futártasak FB06 (400x500x50)", price: 52, quantity: "250 db", description: "Futártasak FB06 Mérete: 400x500x50 mm", image: "image/futartasak.jpg" },
+    { name: "Okmánytasak A/5", price: 20, quantity: "700 db", description: "Okmánytasak A/5", image: "image/okmanytasak.jpg" },
+    { name: "Okmánytasak A/6", price: 15, quantity: "500 db", description: "Okmánytasak A/6", image: "image/okmanytasak.jpg" },
+
     { name: "Rock munkavédelmi bakancs több méretben", price: 7000, quantity: "7 db", description: "Rock munkavédelmi bakancs, acélkaplis, taplemezes. Minősítés: CE, EN, ISO200345:2011, S1P SRC", image: "image/rock.bakancs.jpg" },
   ];
   
@@ -17,6 +27,8 @@ const products = [
       const productImg = document.createElement('img');
       productImg.src = product.image;
       productImg.alt = product.name;
+      productImg.style.width = '200px';
+      productImg.style.height = '200px';
   
       const productName = document.createElement('div');
       productName.classList.add('product-name');
@@ -59,5 +71,12 @@ const products = [
   
     displayProducts(filteredProducts);
   }
+
+  const logo = document.getElementById('logo');
+
+  logo.addEventListener('click', function() {
+      window.location.reload();
+  });
+
   
   displayProducts(products); // Termékek megjelenítése az oldal betöltésekor
